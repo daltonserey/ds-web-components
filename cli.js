ca7c7c51950c4d8f14681318c3d16dbff484df89
Dalton Serey <daltonserey@gmail.com>
2023-12-21 19:58:06 -0300

    fix: cli

--- cli.js
+++ cli.js
@@ -1,10 +1,26 @@
+
 class CliElement extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.innerHTML = `
+        this.root = this.attachShadow({mode: "open"});
+        this.root.innerHTML = `
+            <style>
+                ds-cli div {
+                    box-shadow: 33px 25px 41px 0px rgba(0,0,0,0.75);
+                    width: 96%;
+                    left: 2%;
+                    right: 2%;
+                    bottom: 1em;
+                    border: 1px solid black;
+                    border-radius: 10px;
+                    padding: 1em 3em;
+                    background: white;
+                    position: fixed;
+                }
+            </style>
             <div>
                 <input type="text" placeholder="digite seu comando aqui">
             </div>
